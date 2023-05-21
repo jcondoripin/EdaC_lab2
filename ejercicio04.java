913b2f3a64ddb8ca84c47f87dfa4c9689a565a86
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-05-21 10:20:51 -0500

    Comentarios del codigo final

--- ejercicio04.java
+++ ejercicio04.java
@@ -9,11 +9,13 @@ public class ejercicio04 {
 
     public static void trianguloRecursivo(int b) {
         if (b > 0) {
-            trianguloRecursivo(b - 1);
+            trianguloRecursivo(b - 1); //uso del metodo recursivamente, disminuyendo la base 
+            /*Como vemos se pone antes el metodo para que asi salga escalonadamente de manera 
+             descendente como se nos pide en el ejercicio*/
             for (int i = 0; i < b; i++) {
-                System.out.print("*");
+                System.out.print("*");//Impresion de asteriscos en cada nivel
             }
-            System.out.println();
+            System.out.println();//Salto de linea
         }
     }
 }
